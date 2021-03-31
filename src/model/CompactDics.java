eafefe289c19ed039bc65ac98567c6b4c70e5235
HOANG QUANG THUAN 20183840 <thuan.hq183840@sis.hust.edu.vn>
2021-03-31 17:52:17 +0700

    update

---
+++ src/model/CompactDics.java
@@ -0,0 +1,31 @@
+package model;
+
+public class CompactDics extends Products {
+    private String time;
+    private Double capacity;
+    private String resolution;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public Double getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Double capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getResolution() {
+        return resolution;
+    }
+
+    public void setResolution(String resolution) {
+        this.resolution = resolution;
+    }
+}
---
+++ src/model/Products.java
@@ -0,0 +1,51 @@
+package model;
+
+import java.util.Date;
+
+public abstract class Products {
+    private String name;
+    private String author;
+    private Date yearOfPublication;
+    private String category;
+    private int price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getYearOfPublication() {
+        return yearOfPublication;
+    }
+
+    public void setYearOfPublication(Date yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
